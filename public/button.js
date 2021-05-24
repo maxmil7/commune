e5e9218b0cb1808ff2035e9326a67da86bf07d8b
maxmil7 <maxmil7@maxmil7s-MacBook-Air.local>
2021-05-23 19:13:33 -0700

    minor css fixes

--- public/button.js
+++ public/button.js
@@ -30,13 +30,13 @@ class List extends React.Component {
   render() {
     const { members=[] } = this.state;
     return (
-      <div class = 'max-h-screen p-5 flex-col bg-yellow-100 text-lg'>
-        <h1 class='text-center font-semibold text-blue-600'>Commune</h1>
+      <div class = 'max-h-screen p-5 flex-col bg-yellow-100'>
+        <h1 class='text-center font-semibold text-blue-600 text-3xl'>Commune</h1>
 
         { members.map(e => {
           const tel = e.phone;
           const telLink = `tel:${tel}`;
-          return <div class='border-2 p-5 my-5 bg-yellow-500 rounded-xl'>
+          return <div class='border-2 p-5 my-5 bg-yellow-500 rounded-xl text-xl'>
             <div class='font-bold'>{ e.name }</div>
             <div>{ e.category }</div>
             <div class='text-blue-900'>
